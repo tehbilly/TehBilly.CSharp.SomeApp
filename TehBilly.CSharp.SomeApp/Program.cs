d07663d0a759552682b0dac466a89768492823bc
William McGann <contact@williammcgann.com>
2023-11-14 14:59:56 -0500

    Initial commit

---
+++ TehBilly.CSharp.SomeApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TehBilly.CSharp.SomeLibrary;
+
+ISomeLibrary lib = new SomeLibrary();
+
+Console.WriteLine($"SomeLibrary version: {lib.Version()}");
+Console.WriteLine($"Flipping the string 'Hello, World!': {lib.FlipAString("Hello, World!")}");
